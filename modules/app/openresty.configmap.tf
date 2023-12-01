d0c9546797184daf15f6f68361eeecf24d1898e0
paylm <paylm@users.noreply.github.com>
2023-12-01 11:42:58 +0000

    chore:  update openresty location custom (#5035)

--- modules/app/openresty.configmap.tf
+++ modules/app/openresty.configmap.tf
@@ -505,7 +505,7 @@ resource "kubernetes_config_map" "openresty_config" {
     }
 
     #Allow embedded routes and cache
-    location ~* ^/(custom|file) {
+    location ~* ^/(custom|file)/ {
       proxy_set_header Host $http_host;
       proxy_set_header X-Real-IP $remote_addr;
       proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
