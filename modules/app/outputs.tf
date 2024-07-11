9a01b8172d477d5e6b769d9ffeb6158765ae4a58
paylm <paylm@users.noreply.github.com>
2024-07-11 09:29:09 +0000

    fix: app ingress ip not found (#5804)

--- modules/app/outputs.tf
+++ modules/app/outputs.tf
@@ -1,7 +1,7 @@
 
 #Output ingress ip
 output "ingress_ip" {
-  value = var.has_load_balancer ? kubernetes_service.openresty_server[0].status[0].load_balancer[0].ingress[0].ip : ""
+  value = var.has_load_balancer ? try(kubernetes_service.openresty_server[0].status[0].load_balancer[0].ingress[0].ip, "") : ""
 }
 
 output "ingress_ip_alternative" {
